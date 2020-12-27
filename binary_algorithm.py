47edf0ff8b75cb3d2ec3d8661da7de435478b9e6
student00010671 <student00010671@gmail.com>
2020-12-28 01:59:35 +0500

    Big O of binary algorithm added

--- binary_algorithm.py
+++ binary_algorithm.py
@@ -1,7 +1,7 @@
 # Binary algorithm works with the sorted list/arrays. It determines the mid point of the list and then if the number
 # does not match guess, it removes half of the list (upper or lower depends on the number location)
-# This step is repeated until the number and guess match or in case the number is not in the list until the lowest guess
-# less than the highest
+# This step is repeated until the number is found or until there are elements in the list
+
 
 def binary_search(array, number):
     lowest = 0  # set the lowest item of the array
@@ -21,5 +21,13 @@ def binary_search(array, number):
 
 test_array = [1, 2, 3, 5, 7, 8, 9, 14, 28, 52]
 
-print(binary_search(test_array, 0))  # prints None
+print(binary_search(test_array, 4)) # prints None
 print(binary_search(test_array, 28))  # prints 8
+
+
+# binary algorithm is written as ( log2(n) ). Where:
+# 2 is the base, because every time we decrease the list by two
+# n is the number of elements in the array/list
+# if the logarithm equation gives floating number, we round it up to the integer
+
+# That is, in the worst scenario it would take 4 steps to my program to find the number  (log2(10) ≈ 3.32)
